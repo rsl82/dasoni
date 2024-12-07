6d31d2197e9acaebded3d58cb9cf52b93a6e3e41
Ryan Lee <rsl82@rutgers.edu>
2024-12-07 15:10:06 +0900

    feat: RDS 연결 및 작동 확인

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './configs/db.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
