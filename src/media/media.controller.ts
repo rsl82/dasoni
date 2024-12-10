5ef0a59878a31f7da8ce964a467cfeb352579b96
Ryan Lee <rsl82@rutgers.edu>
2024-12-10 23:29:27 +0900

    사진 업로드가 인증된 유저만 가능하도록 구현

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -2,12 +2,15 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { MediaService } from './media.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('media')
+@UseGuards(AuthGuard('jwt'))
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
 
